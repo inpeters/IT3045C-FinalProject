ca68df8e06942fa505dab5d5514a1d7913b66b6b
yep-was-taken <herrera.tony922@gmail.com>
2021-08-08 15:57:04 -0400

    Config

--- IT3045C-FinalProject/Startup.cs
+++ IT3045C-FinalProject/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace FinalProject
+namespace IT3045C_FinalProject
 {
     public class Startup
     {
@@ -29,12 +29,12 @@ namespace FinalProject
         {
             services.AddControllers();
             services.AddDbContext<MemberInfo>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("MemberContext")));
+                options.UseSqlServer(Configuration.GetConnectionString("MemberInfo")));
             services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MemberContext ctx)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MemberInfo ctx)
         {
             if (env.IsDevelopment())
             {
